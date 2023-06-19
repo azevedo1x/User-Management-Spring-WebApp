42bab9413ed6a76d6aaa36d967664d2fd034b4e2
gabrielsazevedo <gabriel.azeve04@gmail.com>
2023-06-19 11:39:42 -0300

    autowired update fix

--- src/main/java/programacao/web/controller/UsuarioController.java
+++ src/main/java/programacao/web/controller/UsuarioController.java
@@ -1,11 +1,12 @@
 package programacao.web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
 
 import programacao.web.model.Usuario;
 import programacao.web.repository.UsuarioRepository;
@@ -14,8 +15,8 @@ import programacao.web.repository.UsuarioRepository;
 public class UsuarioController {
 
     private UsuarioRepository ur;
-
-    @Autowired
+    
+    // @Autowired
     public UsuarioController(UsuarioRepository usuarioRepository) {
         this.ur = usuarioRepository;
     }
