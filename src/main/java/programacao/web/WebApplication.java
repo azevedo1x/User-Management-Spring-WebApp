2ea2476bb1e3c92b635c5066c36d9a60c27a3b3e
gabrielsazevedo <gabriel.azeve04@gmail.com>
2023-06-26 14:06:24 -0300

    wip back

--- src/main/java/programacao/web/WebApplication.java
+++ src/main/java/programacao/web/WebApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "programacao.web.repository")
-@EntityScan(basePackages = "programacao.web.model")
 
 public class WebApplication {
 	public static void main(String[] args) {
