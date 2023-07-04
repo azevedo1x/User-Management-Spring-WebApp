19e09f1ac3ec46fbf0f450778567067b8a2d375a
gabrielsazevedo <gabriel.azeve04@gmail.com>
2023-07-04 11:03:02 -0300

    wip add db comment

--- src/main/java/programacao/web/data/Banco.java
+++ src/main/java/programacao/web/data/Banco.java
@@ -11,6 +11,8 @@ import org.springframework.orm.jpa.vendor.Database;
 @Configuration
 public class Banco {
 
+    // conectar banco: java -cp hsqldb.jar org.hsqldb.server.Server --database.0 file:mydb --dbname.0 banco
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
