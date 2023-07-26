28e2a6ae3c5a0f2fb35e4b83515b521b7ded376f
gabrielsazevedo <gabriel.azeve04@gmail.com>
2023-07-26 16:27:45 -0300

    WIP update done, TODO: remove

--- src/main/java/programacao/web/controller/UsuarioController.java
+++ src/main/java/programacao/web/controller/UsuarioController.java
@@ -19,20 +19,21 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository ur;
 
-    @RequestMapping(value = "/cadastrarUsuario", method = RequestMethod.GET)
+    @RequestMapping(value = { "/cadastrarUsuario", "/editarUsuario" }, method = RequestMethod.GET)
     public String exibirForm(Model model) {
 
         model.addAttribute("usuario", new Usuario());
+
         return "formUsuario";
 
     }
 
     @RequestMapping(value = "/cadastrarUsuario", method = RequestMethod.POST)
-    public String processarForm(Usuario usuario, Model model) {
+    public String processarFormCadastro(Usuario usuario, Model model) {
 
         try {
 
-            verificarUsuario(usuario);
+            verificarUsuarioCadastro(usuario);
 
         } catch (Exception e) {
 
@@ -42,7 +43,43 @@ public class UsuarioController {
         }
 
         ur.save(usuario);
+
         return "redirect:/cadastrarUsuario";
+
+    }
+
+    @RequestMapping(value = "/editarUsuario", method = RequestMethod.POST)
+    public String processarFormEdicao(Usuario usuario, Model model) throws Excecao {
+
+        Usuario usuarioExistente = ur.findByLogin(usuario.getLogin());
+
+        if (usuarioExistente != null) {
+
+            usuarioExistente.setNome(usuario.getNome());
+            usuarioExistente.setEmail(usuario.getEmail());
+            usuarioExistente.setSenha(usuario.getSenha());
+
+            try {
+
+                verificarUsuarioEdicao(usuario);
+
+            } catch (Exception e) {
+
+                model.addAttribute("erro", e.getMessage());
+                return "formUsuario";
+
+            }
+
+            ur.save(usuarioExistente);
+
+        } else {
+
+            model.addAttribute("erro", "Usuário não encontrado.");
+            return "formUsuario";
+
+        }
+
+        return "redirect:/editarUsuario";
 
     }
 
@@ -56,7 +93,7 @@ public class UsuarioController {
 
     }
 
-    public void verificarUsuario(Usuario usuario) throws Excecao {
+    public void verificarUsuarioCadastro(Usuario usuario) throws Excecao {
 
         List<String> mensagensExcecao = new ArrayList<>();
         Usuario usuarioExistente = ur.findByLogin(usuario.getLogin());
@@ -96,6 +133,42 @@ public class UsuarioController {
             throw new Excecao(String.join(" ", mensagensExcecao));
 
         }
+    }
+
+    public void verificarUsuarioEdicao(Usuario usuario) throws Excecao {
+
+        List<String> mensagensExcecao = new ArrayList<>();
+
+        if (!usuario.getEmail().equals(usuario.getEmailconfirma())) {
+
+            mensagensExcecao.add("E-mails não coincidem.");
+
+        }
+
+        if (usuario.getSenha().length() < 4 || usuario.getSenha().length() > 8) {
+
+            mensagensExcecao.add("Sua senha deve ter um mínimo de 4 caracteres e um máximo de 8 caracteres.");
+
+        }
+
+        if (usuario.getSenha().equals(usuario.getLogin())) {
+
+            mensagensExcecao.add("A senha não pode ser igual ao login.");
+
+        }
+
+        if (!usuario.getSenha().equals(usuario.getSenhaconfirma())) {
+
+            mensagensExcecao.add("Senhas não coincidem.");
+
+        }
+
+        if (!mensagensExcecao.isEmpty()) {
+
+            throw new Excecao(String.join(" ", mensagensExcecao));
+
+        }
+
     }
 
 }
