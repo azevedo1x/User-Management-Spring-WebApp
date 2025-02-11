be926c9c9dde5b102fe507ed66b4a071f5f6fed1
Gabriel Azevedo <gabriel.azevedo@branef.com.br>
2025-02-11 17:45:16 -0300

    algumas mudancas estruturais

---
+++ src/main/java/programacao/web/exception/UsuarioException.java
@@ -0,0 +1,11 @@
+package programacao.web.exception;
+
+public class UsuarioException extends Exception {
+    public UsuarioException() {
+        super();
+    }
+
+    public UsuarioException(String mensagem) {
+        super(mensagem);
+    }
+}
